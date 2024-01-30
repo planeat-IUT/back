c44090ab3cb709448d8d71e54c34d5513881b4cf
Remi.mhx <remimahieux700@gmail.com>
2024-01-30 17:15:32 +0100

    add login register logout

--- src/Controller/UtilisateurController.php
+++ src/Controller/UtilisateurController.php
@@ -2,6 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Utilisateur;
+use App\Form\RegistrationFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 final class UtilisateurController extends \Symfony\Bundle\FrameworkBundle\Controller\AbstractController
@@ -12,17 +19,52 @@ final class UtilisateurController extends \Symfony\Bundle\FrameworkBundle\Contro
     )
     {}
 
-    public function index(){
+    #[Route('/register', name: 'app_register')]
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
+    {
+        $user = new Utilisateur();
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // encode the plain password
+            $user->setPassword(
+                $userPasswordHasher->hashPassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                )
+            );
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+            // do anything else you need here, like send an email
+
+            return $this->redirectToRoute('_profiler_home');
+        }
+
+        return $this->render('utilisateur/register.html.twig', [
+            'registrationForm' => $form->createView(),
+        ]);
     }
 
-    public function login(AuthenticationUtils $authenticationUtils)
+    #[Route(path: '/login', name: 'app_login')]
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        if($this->getUser())
-        {
-            dd('connecté');
-        }
+        // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
+        return $this->render('utilisateur/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error,
+        ]);
     }
+
+    #[Route(path: '/logout', name: 'app_logout')]
+    public function logout(): void
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
 }
